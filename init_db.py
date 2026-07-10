1498c7cdefe3859788bea0150b2d04df55e3c47e
manjunath9446 <manju.r.k9446@gmail.com>
2026-07-11 00:19:38 +0530

    render_updatee1

--- init_db.py
+++ init_db.py
@@ -1,20 +1,12 @@
-from sqlalchemy import text
-
 async def init_db():
 
+    # Import all models so SQLAlchemy registers them
     import src.models.project
     import src.models.summary
     import src.models.comment
     import src.models.project_analysis
 
     async with engine.begin() as conn:
-
         await conn.run_sync(Base.metadata.create_all)
-
-        result = await conn.execute(
-            text("SELECT name FROM sqlite_master WHERE type='table';")
-        )
-
-        print("Tables:", result.fetchall())
 
     print("✅ Database initialized.")
